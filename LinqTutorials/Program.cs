fd52a52d4e7908c91909f921313da2c69be78be0
Subtelny <sebo.janda@gmail.com>
2024-05-24 23:34:40 +0200

    Initialize project

---
+++ LinqTutorials/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace LinqTutorials
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var t = LinqTasks.Task1();
+            var t1 = LinqTasks.Task2();
+            var t2 = LinqTasks.Task3();
+            var t3 = LinqTasks.Task4();
+            var t4 = LinqTasks.Task5();
+            var t5 = LinqTasks.Task6();
+            var t6 = LinqTasks.Task7();
+            var t7 = LinqTasks.Task8();
+            var t8 = LinqTasks.Task9();
+            var t9 = LinqTasks.Task10();
+            var t10 = LinqTasks.Task11();
+            var t11 = LinqTasks.Task12();
+            var t12 = LinqTasks.Task13(new[] { 1, 1, 1, 1, 1, 1, 10, 1, 1, 1, 1 });
+            var t13 = LinqTasks.Task14();
+            var t14 = LinqTasks.Task15();
+            var t15 = LinqTasks.Task16();
+        }
+    }
+}
